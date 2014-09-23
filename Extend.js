f9613769980cdadf57d1a4e0c75f93f57bfe04ea
Nicolas Aragon <nico.diaz.aragon@gmail.com>
2014-09-23 09:46:15 -0500

    cleaning up

--- Extend.js
+++ Extend.js
@@ -22,33 +22,3 @@ function extend(BaseClass, SubClass) {
     return newClassDefinition;
 }
 module.exports = extend;
-////////////////////////////
-////////////////////////////
-////////////////////////////
-///sample code
-////////////////////////////
-////////////////////////////
-////////////////////////////
-function Shape() {
-    Log.Message('Shape Constructor ran');
-};
-
-function TwoDShape() {
-    Log.Message('TwoDShape Constructor ran');
-};
-TwoDShape = extend(Shape, TwoDShape);
-
-function ThreeDShape() {
-    Log.Message('ThreeDShape Constructor ran');
-};
-ThreeDShape = extend(TwoDShape, ThreeDShape);
-var a3DShape = new ThreeDShape();
-//prints: "Shape Constructor ran... TwoDShape Constructor ran.... ThreeDShape Constructor ran"
-//and also initializes all instance members just like they were meant by the superclass implementation
-//a3DShape has access to every member-function/method in both Shape and TwoDShape and may override methods and gain access to the superclass definition in spite of the overrides. And it will also feature properties for any member variable be it an object or an array:
-TwoDShape.prototype.toString = function () {
-    return TwoDShape.prototype.uber.toString.call(this) + " " + this.name;
-};
-ThreeDShape.prototype.toString = function () {
-    return ThreeDShape.prototype.uber.toString.call(this) + " " + this.name;
-};
